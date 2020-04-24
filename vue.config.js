6eddae4cc04b81aaddd6521591c87259d7429aba
Thomas Chopitea <tomchop@gmail.com>
2020-04-24 20:04:16 +0200

    Add reverse proxy config for dev

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    // see https://cli.vuejs.org/config/#devserver for more options
+    proxy: {
+      '^/api': {
+        autoRewrite: true,
+        target: 'http://localhost:5000/'
+      }
+    }
+  }
+}
