c11d4c9e3d21659995e2be0aa3ad4616d4678f84
Thomas Chopitea <tomchop@gmail.com>
2020-04-24 22:14:04 +0200

    Changes in entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 if [[ "$1" =~ "dev" ]]; then
     if [[ ! -d "node_modules" ]]; then
-     npm install
+      npm install
     fi
-    if [[ -z "${VUE_API_BACKEND}" ]]; then
-      sed -i .bak s/localhost/${VUE_API_BACKEND}/ vue.config.js
+    if [[ ! -z "${VUE_API_BACKEND}" ]]; then
+      # this is the busybox version of sed
+      sed -i s/localhost/${VUE_API_BACKEND}/ vue.config.js
+    fi
     npm run serve
 fi
 
