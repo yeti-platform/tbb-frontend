2ddaa89c2fb6dfa6d43625e18a64937908d130f8
Thomas Chopitea <tomchop@gmail.com>
2020-04-24 21:29:15 +0200

    Add vue config and entrypoint change

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,8 @@ if [[ "$1" =~ "dev" ]]; then
     if [[ ! -d "node_modules" ]]; then
      npm install
     fi
+    if [[ -z "${VUE_API_BACKEND}" ]]; then
+      sed -i .bak s/localhost/${VUE_API_BACKEND}/ vue.config.js
     npm run serve
 fi
 
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    // see https://cli.vuejs.org/config/#devserver for more options
+    proxy: 'http://localhost:5000/'
+  }
+}
