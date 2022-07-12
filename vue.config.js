df74198387c344af253db53a7bd98b92e4b38683
Thomas Chopitea <tomchop@gmail.com>
2022-07-12 21:05:47 +0200

    Proxy to 127.0.0.1

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     proxy: {
       "^/api": {
         autoRewrite: true,
-        target: "http://localhost:5000/"
+        target: "http://127.0.0.1:5000/"
       }
     }
   }
