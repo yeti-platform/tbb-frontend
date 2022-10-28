e4451c9b3caca0a36b5c006ae677d3237615f20c
Thomas Chopitea <tomchop@gmail.com>
2022-10-28 17:53:49 +0200

    Fix docker things

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,11 +3,13 @@ set -e
 
 if [[ "$1" =~ "dev" ]]; then
     if [[ ! -d "node_modules" ]]; then
+    # This needs to live here if we want node_modules to be actually shipped
+    # to the host filesystem
       npm install
     fi
     if [[ ! -z "${VUE_API_BACKEND}" ]]; then
       # this is the busybox version of sed
-      sed -i s/localhost/${VUE_API_BACKEND}/ vue.config.js
+      sed -i s/127.0.0.1/${VUE_API_BACKEND}/ vue.config.js
     fi
     npm run serve
 fi
